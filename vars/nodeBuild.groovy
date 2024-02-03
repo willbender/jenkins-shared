8c66407d1c8ebddaff46de12fe6c398894d5c923
William Bernal <william.bernal@gmail.com>
2024-02-03 07:07:15 +0100

    Update nodeBuild.groovy

--- vars/nodeBuild.groovy
+++ vars/nodeBuild.groovy
@@ -53,14 +53,10 @@ def call(Map arguments=[:]) {
             }
             //Scan docker image vulnerabilities
             stage('Scan Docker Image for Vulnerabilities'){
-                agent {
-                    docker {
-                        image 'aquasec/trivy:0.49.0'
-                    }
-                }
+                agent any
                 steps{
                     script{
-                        def vulnerabilities = sh(script: "trivy image --exit-code 0 --severity HIGH,MEDIUM,LOW --no-progress ${registry}:${env.BRANCH_NAME}", returnStdout: true).trim()
+                        def vulnerabilities = sh(script: "docker run aquasec/trivy:0.49.0 image --exit-code 0 --severity HIGH,MEDIUM,LOW --no-progress ${registry}:${env.BRANCH_NAME}", returnStdout: true).trim()
                         echo "Vulnerability Report:\n${vulnerabilities}"
                     }
                 }
